f895f5f675348d7288101f2100ea3bf07870c47e
suzhelan <sacz@163.com>
2025-11-26 21:14:54 +0800

    docs:Add a health check note

--- src/main/java/top/suzhelan/nacosgateway/controller/HealthController.java
+++ src/main/java/top/suzhelan/nacosgateway/controller/HealthController.java
@@ -7,19 +7,24 @@ import top.suzhelan.nacosgateway.entity.CommonResult;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 健康检查，网关自身也是微服务，所以需要提供健康检查接口
+ */
 @RestController
 public class HealthController {
 
+    /**
+     * 健康检查接口 能访问得通就行
+     * @return CommonResult
+     */
     @GetMapping("/actuator/health")
-    public CommonResult<Map<String, Object>> health() {
+    public Map<String, Object> health() {
         Map<String, Object> healthInfo = new HashMap<>();
         healthInfo.put("status", "UP");
         healthInfo.put("service", "NacosGateway");
-        
         Map<String, Object> details = new HashMap<>();
         details.put("timestamp", System.currentTimeMillis());
         healthInfo.put("details", details);
-        
-        return CommonResult.success(healthInfo);
+        return healthInfo;
     }
 }
